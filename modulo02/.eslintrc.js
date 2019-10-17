4e91410a6f7ac1b7ad4d56669516d5d75d30460e
Eduardo Paixao Viccari <eduardo@MacBook-Air-de-Eduardo.local>
2019-10-17 20:45:15 -0300

    eslint adjusts

--- modulo02/.eslintrc.js
+++ modulo02/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'airbnb-base',
     'prettier'
   ],
-  plugins: ['prettier'], 
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
