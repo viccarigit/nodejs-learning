e0484739a5c5def1eea8a1e14b90653550d4905c
Eduardo Paixao Viccari <eduardo@MacBook-Air-de-Eduardo.local>
2019-10-19 14:08:47 -0300

    adjusts

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -1,6 +1,11 @@
 import User from '../models/User';
 
 class UserController {
+  /**
+   * save the new user on database
+   * @param {*} req
+   * @param {*} res
+   */
   async store(req, res) {
     const userExists = await User.findOne({
       where: { email: req.body.email },
@@ -13,6 +18,38 @@ class UserController {
     const { id, name, email, provider } = await User.create(req.body);
 
     return res.send({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  /**
+   * update user on databse (optionaly password, if oldPassword has informed)
+   * @param {*} req
+   * @param {*} res
+   */
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
       id,
       name,
       email,
