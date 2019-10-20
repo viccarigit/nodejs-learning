29ce4a17f6bbb465d157e558f1be00da379a2ef7
Eduardo Paixao Viccari <eduardo@MacBook-Air-de-Eduardo.local>
2019-10-20 19:52:16 -0300

    continuous

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -4,6 +4,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,11 +17,9 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // after that, all reqs are authenticated
-
 routes.put('/users', UserController.update);
-
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
