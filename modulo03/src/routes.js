d0679e207ba52e802fc3711223cd48a253d4cbe9
Eduardo Paixao Viccari <eduardo@MacBook-Air-de-Eduardo.local>
2019-10-25 21:05:45 -0300

    initial commit

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,12 +21,18 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // after that, all reqs are authenticated
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
 routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/schedules', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
